10221847e5403346f333b1c36c35e1767ccb376b
ahmad <farag.4@osu.edu>
2014-02-20 17:14:43 -0500

    work from wednesday night

--- TBI-Interview/src/com/tbi_id/SettingsActivity.java
+++ TBI-Interview/src/com/tbi_id/SettingsActivity.java
@@ -4,33 +4,57 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
 
- @Override
- protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-  super.onCreate(savedInstanceState);
-  
-  this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-	this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-			WindowManager.LayoutParams.FLAG_FULLSCREEN);
-  setContentView(R.layout.settings);
+		super.onCreate(savedInstanceState);
 
-  SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-  StringBuilder builder = new StringBuilder();
+		setContentView(R.layout.settings);
 
-  builder.append("\n" + sharedPrefs.getString("pref_user_profile", "OSU"));
-  builder.append("\n" + sharedPrefs.getString("pref_hippa_setting", "Yes"));
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		
+		EditText enterEmailHipaa = (EditText) findViewById(R.id.emailEnterHipaa);
+		TextView emailNotif = (TextView) findViewById(R.id.enterEmailNotif);
 
-  TextView settingsTextView = (TextView) findViewById(R.id.settings_text_view);
-  settingsTextView.setText(builder.toString());
+		Boolean checked = sharedPrefs.getBoolean("checkboxHipaa", false);
+		if (checked == false) {
+			enterEmailHipaa.setVisibility(View.GONE);
+			emailNotif.setVisibility(View.GONE);
 
- }
+		}
+		
 
+		else
+
+		{
+			SharedPreferences.Editor editor = sharedPrefs.edit();
+			String email = sharedPrefs.getString("hipaaEmail", "ahmad.k.farag@gmail.com");
+			enterEmailHipaa.setText(email);
+			
+
+		}
+
+		
+		
+		
+		CheckBox checkBoxHipaa = (CheckBox) findViewById(R.id.hippaCompliance);
+		checkBoxHipaa.add
+		
+		
+		
+	}
 }
-
